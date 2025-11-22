fc18ed14b17f90cde5266122ba335f6961d0b43c
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-11-22 23:19:34 +0000

    tests for better_swqos

--- rebuild.sh
+++ rebuild.sh
@@ -6,9 +6,9 @@ cargo build --release
 cd ../
 TX_SIZE=1000
 N=24
-C=1
+C=5
 T=5.0
-export RUST_LOG="info,solana_streamer=debug"
-sudo --preserve-env=RUST_LOG  ./main.py solana_pubkeys.txt  --duration=$T --num_connections=$C  --latency=$1  --tx-size $TX_SIZE --num_clients $N
+#export RUST_LOG="info,solana_streamer=debug"
+sudo --preserve-env=RUST_LOG  ./main.py solana_pubkeys.txt --max-tps 100000  --duration=$T --num_connections=$C  --latency=$1  --tx-size $TX_SIZE --num_clients $N
 ./plot_timelapse_data.py 
 
